cedf5a4ee62489efd713b705f2f0f2d4cca1cbd9
iotam <150601397+iotamg@users.noreply.github.com>
2025-11-26 08:49:33 +0000

    Start

--- sql.py
+++ sql.py
@@ -2,5 +2,7 @@ import sqlite3
 try:
   conn = sqlite3.connect('example.db')  # Creates a new database file if it doesn’t exist
   cursor = conn.cursor()
+  stmt="CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)"
 except:
+   print("Error")
   
