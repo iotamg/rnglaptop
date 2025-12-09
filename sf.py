a7c05adb0577d7150b59d0a3977780622d70b244
iotam <150601397+iotamg@users.noreply.github.com>
2025-12-09 07:35:52 +0000

    changed the way to run ngrok, should run now in a new window to not distube the main process (sf.py)

--- sf.py
+++ sf.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
 import pymysql
-import os
+import subprocess
 
 app = Flask(__name__)
-os.system("ngrok http --domain=linette-exudative-delorse.ngrok-free.dev 5000") # start ngrok public url forwarding to localhost:5000
+subprocess.Popen(["lxterminal", "-e", "ngrok http --domain=linette-exudative-delorse.ngrok-free.dev 5000"]) # start ngrok public url forwarding to localhost:5000
 db_conn = pymysql.connect(
     host="localhost",
     user="root",
